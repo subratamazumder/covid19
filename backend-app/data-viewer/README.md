0e2448228e6bb85e432631aaf9e037b6360cf601
Subrata Mazumder <subrata.besu@gmail.com>
2020-03-31 22:07:55 +0530

    api load test added

---
+++ backend-app/data-viewer/README.md
@@ -0,0 +1,19 @@
+# Run API Load Test
+
+## Setup Test Scenarios
+- Edit `data-viewer-load.yaml` update `config:target` with AWS API Gateway Host
+- If needed, update `config:variables:country` to add more countries in your test
+
+## Execute Test
+```console
+$ sudo npm install -g artillery
+
+$ sudo npm install -g artillery-plugin-expect
+
+$ cd covid19/backend-app/data-viewer/api-tests
+
+$ ./run-load-test.sh <api-key> <duration> <arrivalRate>
+
+```
+# Reference
+https://artillery.io/docs/cli-reference/
---
+++ backend-app/data-viewer/api-tests/run-load-test.sh
@@ -0,0 +1,28 @@
+#usage : $./run-load-test.sh <api-key> <duration> <arrivalRate>
+export API_KEY=$1
+if [ -z "$1" ]
+  then
+    echo "No <api-key> supplied, skip running test"
+    exit 1
+fi
+export DURATION=$2
+if [ -z "$2" ]
+  then
+    echo "No <duration> supplied, defaulting to 1"
+    export DURATION=1
+fi
+export ARRIVAL_RATE=$3
+if [ -z "$3" ]
+  then
+    echo "No <arrivalRate> supplied, defaulting to 1"
+    export ARRIVAL_RATE=1
+fi
+#export OVERRIDE_JSON_TEMPLATE='{"config": {"defaults": {"headers": {"x-api-key": "#API_KEY#"}}}}'
+export OVERRIDE_JSON_TEMPLATE='{ "config": { "phases": [ { "duration": #DURATION#, "arrivalRate": #ARRIVAL_RATE# } ], "defaults": { "headers": { "x-api-key": "#API_KEY#", "Accept": "application/json" } } } }'
+export OVERRIDE_JSON=${OVERRIDE_JSON_TEMPLATE//#API_KEY#/$API_KEY}
+export OVERRIDE_JSON=${OVERRIDE_JSON//#DURATION#/$DURATION}
+export OVERRIDE_JSON=${OVERRIDE_JSON//#ARRIVAL_RATE#/$ARRIVAL_RATE}
+echo $OVERRIDE_JSON
+#DEBUG=http:request artillery run --overrides '{"config": {"phases": [{"duration": 2, "arrivalRate": 1}]}}' artillery-test/data-viewer-load.yaml
+#DEBUG=http:response artillery run --overrides "$OVERRIDE_JSON" artillery-test/data-viewer-load.yaml
+# artillery run --overrides "$OVERRIDE_JSON" artillery-test/data-viewer-load.yaml
